5de318732df34009c9969cb0e970574e68854ce1
CroaLaAisha <45094728+CroaLaAisha@users.noreply.github.com>
2020-08-20 16:40:10 +0900

    2.16 Local Varibles in Pug

--- app.js
+++ app.js
@@ -3,25 +3,26 @@ import morgan from "morgan";
 import helmet from "helmet";
 import cookieParser from "cookie-parser";
 import bodyParser from "body-parser";
-import { userRouter } from "./router";
+import { localsMiddleware } from "./middlewares";
+import routes from "./routes";
+import userRouter from "./routers/userRouter";
+import videoRouter from "./routers/videoRouter";
+import globalRouter from "./routers/globalRouter";
+
 const app = express();
 
-const handleHome = (req, res) => res.send("Hello from my ass");
-
-const handleProfile = (req, res) => res.send("You are on my profile");
-
-/////////////////////////////* middlewars */////////////////////////////
+//////////////////////* middlewars *//////////////////////
+app.use(helmet());
+app.set("view engine", "pug");
 app.use(cookieParser());
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(helmet());
 app.use(morgan("dev"));
-/////////////////////////////////////////////////////////////////////////
 
-app.get("/", handleHome);
+app.use(localsMiddleware);
 
-app.get("/profile", handleProfile);
-
-app.use("/user", userRouter);
+app.use(routes.home, globalRouter);
+app.use(routes.users, userRouter);
+app.use(routes.videos, videoRouter);
 
 export default app;
