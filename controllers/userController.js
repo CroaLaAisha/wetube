d7296d99ed88ca761eae45feb071ae368ebab76a
CroaLaAisha <45094728+CroaLaAisha@users.noreply.github.com>
2020-08-20 20:38:35 +0900

    2.17 Template Variables in Pug
    2020-08-20 20:38 studed

--- controllers/userController.js
+++ controllers/userController.js
@@ -1,7 +1,10 @@
-export const join = (req, res) => res.send("Join");
-export const login = (req, res) => res.send("Login");
-export const logout = (req, res) => res.send("Logout");
-export const users = (req, res) => res.send("users");
-export const userDetail = (req, res) => res.send("userDetail");
-export const editProfile = (req, res) => res.send("editProfile");
-export const changePassword = (req, res) => res.send("changePassword");
+export const join = (req, res) => res.send("Join", { pageTitle: "Join" });
+export const login = (req, res) => res.send("Login", { pageTitle: "Login" });
+export const logout = (req, res) => res.send("Logout", { pageTitle: "Logout" });
+export const users = (req, res) => res.send("users", { pageTitle: "Users" });
+export const userDetail = (req, res) =>
+  res.send("userDetail", { pageTitle: "User Detail" });
+export const editProfile = (req, res) =>
+  res.send("editProfile", { pageTitle: "Edit Profile" });
+export const changePassword = (req, res) =>
+  res.send("changePassword", { pageTitle: "Change Password" });
